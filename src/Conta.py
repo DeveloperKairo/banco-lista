be78b9b6b864000144ad3840bf09f4bdf8b45f2f
DeveloperKairo <contatodevkairo@gmail.com>
2025-11-16 18:12:14 -0300

    refactor class conta

--- src/Conta.py
+++ src/Conta.py
@@ -1,31 +1,40 @@
 class Conta:
-  def __init__ (self, numero, titular, saldo, tipo_conta):
-    self.numero = numero
-    self.titular = titular
-    self.saldo = saldo
-    self.tipo_conta = tipo_conta
+    def __init__(self, numero, titular, saldo, limite, data_abertura, tipo_conta):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+        self.data_abertura = data_abertura
+        self.tipo_conta = tipo_conta
 
-    self.extrato = []
+        self.historico_transacoes = []
 
-  def depositar(self, valor):
-    self.saldo += valor
-    self.extrato.append(f'Depósito: +{valor}')
+    def registrar(self, mensagem):
+        self.historico_transacoes.append(mensagem)
 
-  def sacar(self, valor):
-    if valor < self.saldo:
-      self.saldo -= valor
-      self.extrato.append(f'Saque: -{valor}')
-    else:
-      print("Saldo insuficiente!")
+    def depositar(self, valor):
+        self.saldo += valor
+        self.registrar(f"Depósito: +{valor}")
+        return True
 
-  def transferir(self, valor, conta_destino):
-    if valor < self.saldo:
-      self.saldo -= valor
-      conta_destino.saldo += valor
-      self.extrato.append(f'Transferência: -{valor} para conta {conta_destino.numero}')
-    else:
-      print("Saldo insuficiente!")
+    def sacar(self, valor):
+        if valor > self.saldo + self.limite:
+            return False
+        self.saldo -= valor
+        self.registrar(f"Saque: -{valor}")
+        return True
 
-  def consultar_saldo(self, saldo):
-    print(f'Saldo da conta {self.numero}: {self.saldo}')
-    
+    def transferir(self, valor, conta_destino):
+        if valor > self.saldo + self.limite:
+            return False
+
+        self.saldo -= valor
+        conta_destino.saldo += valor
+
+        self.registrar(f"Transferência: -{valor} para {conta_destino.numero}")
+        conta_destino.registrar(f"Transferência: +{valor} de {self.numero}")
+
+        return True
+
+    def consultar_saldo(self):
+        print(f"Saldo: R${self.saldo:.2f}")
